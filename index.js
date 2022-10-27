5a2f30f187c1f5c46d11ee90b80dfce321c585c6
ishratesha <ishratjahan22222@gmail.com>
2022-10-27 23:30:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const categories = require('./data/categories.json');
+const coursedetails = require('./data/courseinfo.json');
+
+app.get('/', (req, res) => {
+    res.send('ok');
+});
+//categories 
+app.get('/categories', (req, res) => {
+    res.send(categories)
+});
+//find via catagoryid 
+app.get('/category/:id', (req, res) => {
+        const id = req.params.id;
+         if (id === '06') {
+            res.send();
+        }
+        else {
+            const category_courses = coursedetails.filter(n => n.category_id === id);
+            res.send(category_courses);
+        }
+})
+//course 
+app.get('/course/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(req.params);
+    const selectedCourse = coursedetails.find(n => n._id === id);
+     res.send(selectedCourse);
+})
+// app.get('/category/:id', (req, res) => {
+//     const id = req.params.id;
+//     if (id === '07') {
+//         res.send();
+//     }
+//     else {
+//         const category_courses = coursedetails.filter(n => n.category_id === id);
+//         res.send(category_courses);
+//     }
+// })
+
+
+// app.get('/coursedetails', (req, res) =>{
+//     res.send(coursedetails);
+// });
+
+// app.get('/coursedetails/:id', (req, res) => {
+//     const id = req.params.id;
+  ////  const selectedNews = coursedetails.find(n => n._id === id);
+     //res.send(selectedNews);
+// });
+
+app.listen(port, () => {
+    console.log('Server running on port', port);
+})
